f793e6a7a706bffde2d4e8b269aa021339eed503
Saikumar Majjari <saikumar.majjarii@gmail.com>
2023-01-18 20:36:28 +0530

    Commit by Saikumar Majjari

---
+++ src/test/java/Selenium_Dummy_Folder/PrintMsgs.java
@@ -0,0 +1,23 @@
+package Selenium_Dummy_Folder;
+
+public class PrintMsgs {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		PrintMsgs Msg = new PrintMsgs();
+		
+		Msg.Click();
+
+	}
+	
+	
+	
+	public void Click()
+	{
+		
+		System.out.println("The CLick is Happened");
+		
+	}
+
+}
